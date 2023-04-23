06ab441dc8a7e141003e1886e32e344ab4c52fd3
Dani Ocaranza <docaranzan@gmail.com>
2023-04-23 00:36:59 -0400

    update export

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,10 @@ extends CharachterBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	@export var speed:int = 20
+	var speed:int = 20
 	var velocity:Vector2 = Vector2.ZERO
 	var friction:float = 0.8
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	var axis_y
